c9905b162170ae7265b90110cc560534705661a8
pedrinho-devv <pedrodesousakiske@gmail.com>
2025-05-02 11:58:10 -0300

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,12 +46,17 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
-        {/* Adicionando meta tags de SEO e Open Graph */}
+        {/* Metadados de SEO e Open Graph */}
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="theme-color" content="#0C2433" />
+        
+        {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
         <meta name="robots" content="index, follow" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
